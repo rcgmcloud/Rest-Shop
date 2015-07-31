80d9c6f21d2c2e9a7ceec0791f5c9722be02091f
Raymond Mariano <rcm@hawaii.edu>
2015-07-30 21:17:44 -1000

    add order routes

--- seed/products.js
+++ seed/products.js
@@ -1,12 +1,14 @@
 var models = require('../models');
 var Product = models.Product;
+var Inventory = models.Inventory;
 var faker = require('faker');
 
 models.sequelize
   .sync({force:true})
   .then(function(){
     var productsArr = [];
-    for(var i=0; i < 10; i++){
+    // TO CREATE RANDOM ITEMS
+    for(var i=0; i < 4; i++){
       productsArr.push({
       name: faker.commerce.productName(),
       description: "It's " + faker.commerce.productAdjective() + " and "+ faker.commerce.productAdjective() + "!",
@@ -14,4 +16,26 @@ models.sequelize
     });
     }
     return Product.bulkCreate(productsArr);
+  })
+    //fill inventory table
+  .then(function(){
+    var inventoryArr = [
+      {
+        quantity: 25,
+        product_id: 1
+      },
+      {
+        quantity: 25,
+        product_id: 2
+      },
+      {
+        quantity: 25,
+        product_id: 3
+      },
+      {
+        quantity: 25,
+        product_id: 4
+      }
+    ];
+    return Inventory.bulkCreate(inventoryArr);
 });
--- server.js
+++ server.js
@@ -4,6 +4,8 @@ var app = express();
 var pg = require('pg');
 var hstore = require('pg-hstore')();
 var models = require('./models');
+var path = require('path');
+var bodyParser = require('body-parser');
 
 var restify = require('restify');
 var server = restify.createServer({
@@ -15,6 +17,7 @@ server.use(restify.acceptParser(server.acceptable));
 server.use(restify.queryParser());
 server.use(restify.bodyParser());
 
+//show all products
 server.get('/products', function (req, res, next) {
   models.Product
     .findAll()
@@ -23,6 +26,7 @@ server.get('/products', function (req, res, next) {
     });
 });
 
+//show a single product
 server.get('/products/:id', function (req, res, next) {
   models.Product
     .findById(req.params.id)
@@ -30,6 +34,39 @@ server.get('/products/:id', function (req, res, next) {
       res.render('product_view', {product: product});
     });
 });
+
+//post an order
+server.post('/orders', function (req, res, next) {
+  models.Order
+    .create ({
+      name: req.body.author,
+      product_id: req.body.product,
+      quantity: req.body.quantity
+    })
+    .then (function (order) {
+      //subtract order quantity from inventory
+    })
+  ;
+});
+
+//show orders
+server.get('/orders', function (req, res, next) {
+  models.Order
+    .findAll()
+    .then(function (orders) {
+      res.render('orders', {orders: orders});
+    });
+});
+
+//show single order
+server.get('/orders/:id', function (req, res, next) {
+  models.Order
+    .findById(req.params.id)
+    .then(function (order) {
+      res.render('order_view', {order: order});
+    });
+});
+
 
 server.listen(8080, function () {
   console.log('%s listening at %s', server.name, server.url);
